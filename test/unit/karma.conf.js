2ea023d7b9cc5c95268daa215826a39fc89730c4
Joshua Minzner <josh@minzner.org>
2015-02-04 11:00:08 -0500

    Use 'commonStrict' module format to avoid a 6to5 bug.

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -62,7 +62,8 @@ module.exports = function(config) {
             debug: true,
             transform: [
                 ['6to5ify', {
-                    loose: 'all'
+                    loose: 'all',
+                    modules: 'commonStrict'
                 }],
                 ['partialify']
             ]

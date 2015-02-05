3ac452feac7662b209c96997133cb66031a79aaf
Joshua Minzner <josh@minzner.org>
2015-02-05 11:50:14 -0500

    Add global event dispatcher.

---
+++ test/unit/spec/lib/dispatcher.ut.js
@@ -0,0 +1,19 @@
+describe('dispatcher', function() {
+    import dispatcher from '../../../../lib/dispatcher.js';
+    import {EventEmitter} from 'events';
+
+    beforeEach(function() {
+        spyOn(dispatcher, 'setMaxListeners').and.callThrough();
+
+        dispatcher.constructor();
+        dispatcher.removeAllListeners();
+    });
+
+    it('should exist', function() {
+        expect(dispatcher).toEqual(jasmine.any(EventEmitter));
+    });
+
+    it('should allow 100 listeners', function() {
+        expect(dispatcher.setMaxListeners).toHaveBeenCalledWith(100);
+    });
+});

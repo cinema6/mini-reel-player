db5805211d5a3f70c4e19953258cefc008e385e3
Joshua Minzner <josh@minzner.org>
2015-02-05 12:24:11 -0500

    Add static factory method to Logger.

--- test/unit/spec/lib/logger.ut.js
+++ test/unit/spec/lib/logger.ut.js
@@ -10,22 +10,44 @@ describe('Logger', function() {
         expect(logger).toEqual(jasmine.any(Object));
     });
 
-    describe('methods', function() {
-        ['log', 'info', 'error', 'warn'].forEach(method => {
-            describe(`${method}()`, function() {
-                let object;
+    describe('static', function() {
+        describe('methods', function() {
+            describe('create()', function() {
+                let result;
 
                 beforeEach(function() {
-                    spyOn(console, method);
-                    jasmine.clock().mockDate();
+                    spyOn(Logger.prototype, 'constructor').and.callThrough();
 
-                    object = { name: 'Foo' };
-
-                    logger[method]('This is a test.', object);
+                    result = Logger.create('A Context');
                 });
 
-                it(`should call console.${method}() with decorations`, function() {
-                    expect(console[method]).toHaveBeenCalledWith(`${(new Date()).toISOString()} [${method}] {My Context}`, 'This is a test.', object);
+                it('should return a new logger instance', function() {
+                    expect(result).toEqual(jasmine.any(Logger));
+                });
+            });
+        });
+    });
+
+    describe('instance', function() {
+        describe('methods', function() {
+            ['log', 'info', 'error', 'warn'].forEach(method => {
+                describe(`${method}()`, function() {
+                    let object;
+
+                    beforeEach(function() {
+                        logger = Logger.create('My Context');
+
+                        spyOn(console, method);
+                        jasmine.clock().mockDate();
+
+                        object = { name: 'Foo' };
+
+                        logger[method]('This is a test.', object);
+                    });
+
+                    it(`should call console.${method}() with decorations`, function() {
+                        expect(console[method]).toHaveBeenCalledWith(`${(new Date()).toISOString()} [${method}] {My Context}`, 'This is a test.', object);
+                    });
                 });
             });
         });

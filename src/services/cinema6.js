ec2b6e7a1951b4abb1131a7fdc0871e28ff57afb
Joshua Minzner <josh@minzner.org>
2015-02-09 09:43:57 -0500

    Add cinema6 service for communicating with c6embed via postMessage.

---
+++ src/services/cinema6.js
@@ -0,0 +1,69 @@
+import {EventEmitter} from 'events';
+import postMessage from './post_message.js';
+import global from '../../lib/global.js';
+import {createKey} from 'private-parts';
+import RunnerPromise from '../../lib/RunnerPromise.js';
+import {
+    defer
+} from '../../lib/utils.js';
+
+const _ = createKey();
+
+class Cinema6 extends EventEmitter {
+    constructor() {
+        super();
+
+        this.ready = false;
+
+        _(this).session = defer(RunnerPromise);
+        _(this).appData = defer(RunnerPromise);
+        _(this).options = undefined;
+
+        if (!!global.__karma__) { this.__private__ = _(this); }
+    }
+
+    init(config = {}) {
+        const session = postMessage.createSession(global.parent);
+
+        session.request('handshake').then(handshakeData => {
+            const completeHandshake = () => {
+                this.ready = true;
+                this.emit('ready', true);
+                session.ping('ready', true);
+                _(this).session.fulfill(session);
+            };
+
+            _(this).appData.fulfill(handshakeData.appData);
+
+            let setupResult;
+
+            if (config.setup) {
+                setupResult = config.setup(handshakeData.appData);
+            }
+
+            if (setupResult instanceof Promise) {
+                setupResult.then(completeHandshake);
+            } else {
+                completeHandshake();
+            }
+        });
+
+        _(this).options = config;
+
+        return session;
+    }
+
+    getSession() {
+        return _(this).session.promise;
+    }
+
+    getAppData() {
+        return _(this).appData.promise;
+    }
+
+    fullscreen(bool) {
+        return this.getSession().then(session => session.ping('fullscreenMode', !!bool));
+    }
+}
+
+export default new Cinema6();

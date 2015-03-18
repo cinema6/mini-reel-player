e6bcc0e0592f62a1d586934fd20eaeecdcf624eb
Joshua Minzner <josh@minzner.org>
2015-03-18 08:47:23 -0400

    Add environment object for parsing data about the webpage the player is running on.

---
+++ src/environment.js
@@ -0,0 +1,17 @@
+const c6 = global.c6 || {};
+const $location = (() => {
+    try {
+        return global.parent.location;
+    } catch (e) {
+        return global.location;
+    }
+}());
+
+class Environment {
+    constructor() {
+        this.debug = !!c6.kDebug;
+        this.secure = $location.protocol === 'https:';
+    }
+}
+
+export default new Environment();

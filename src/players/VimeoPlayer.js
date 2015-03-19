9697766cbf8b31cbb6903025bb04637b263eaccd
Joshua Minzner <josh@minzner.org>
2015-03-19 16:58:36 -0400

    Add VimeoPlayer.

---
+++ src/players/VimeoPlayer.js
@@ -0,0 +1,258 @@
+import CorePlayer from './CorePlayer.js';
+import {createKey} from 'private-parts';
+import vimeo from '../services/vimeo.js';
+import browser from '../services/browser.js';
+import Runner from '../../lib/Runner.js';
+
+const _ = createKey();
+
+function getInitialState() {
+    return {
+        ready: false,
+        hasPlayed: false,
+        src: null,
+
+        buffered: 0,
+        currentTime: 0,
+        duration: 0,
+        ended: false,
+        paused: true,
+        readyState: 0,
+        seeking: false,
+        error: null
+    };
+}
+
+export default class VimeoPlayer extends CorePlayer {
+    constructor() {
+        super(...arguments);
+
+        this.autoplay = false;
+        this.start = null;
+        this.end = null;
+
+        _(this).state = getInitialState();
+        _(this).ready = false;
+        _(this).src = null;
+        _(this).iframe = null;
+        _(this).player = null;
+    }
+
+    get buffered() {
+        return _(this).state.buffered;
+    }
+
+    get currentTime() {
+        return _(this).state.currentTime;
+    }
+    set currentTime(value) {
+        const start = this.start || 0;
+        const end = this.end || Infinity;
+        const adjustedTime = Math.min(Math.max(start + value, start), end);
+        const {player, state} = _(this);
+
+        if (!player) {
+            return (state.currentTime = adjustedTime);
+        }
+
+        this.emit('seeking');
+        _(this).state.seeking = true;
+        player.call('seekTo', adjustedTime);
+    }
+
+    get duration() {
+        return _(this).state.duration;
+    }
+
+    get ended() {
+        return _(this).state.ended;
+    }
+
+    get paused() {
+        return _(this).state.paused;
+    }
+
+    get readyState() {
+        return _(this).state.readyState;
+    }
+
+    get seeking() {
+        return _(this).state.seeking;
+    }
+
+    set src(value) {
+        _(this).src = value;
+    }
+    get src() {
+        return _(this).src;
+    }
+
+    get error() {
+        return _(this).state.error;
+    }
+
+    pause() {
+        const {player, state: { ready }} = _(this);
+
+        if (ready) { player.call('pause'); }
+    }
+
+    play() {
+        this.load();
+
+        const {player, state: { ready, hasPlayed }} = _(this);
+        const play = (() => {
+            if (hasPlayed) { return player.call('play'); }
+
+            browser.test('autoplay').then(autoplayable => {
+                if (autoplayable) { player.call('play'); }
+            });
+        });
+
+        if (ready) { play(); } else { player.once('ready', play); }
+    }
+
+    load() {
+        if (_(this).state.src && (this.src !== _(this).state.src)) {
+            this.unload();
+        }
+        _(this).state.src = this.src;
+
+        if (_(this).iframe) { return; }
+
+        const element = this.element || this.create();
+        const iframe = document.createElement('iframe');
+
+        const attr = ((attribute, value = '') => iframe.setAttribute(attribute, value));
+
+        attr('src', `//player.vimeo.com/video/${this.src}?api=1&player_id=${this.id}`);
+        attr('width', '100%');
+        attr('height', '100%');
+        attr('frameborder', '0');
+        attr('webkitAllowFullScreen');
+        attr('mozallowfullscreen');
+        attr('allowFullScreen');
+
+        const player = new vimeo.Player(iframe);
+        const start = this.start || 0;
+        const end = this.end || Infinity;
+
+        const onceLoadProgress = (() => {
+            this.emit('canplay');
+            this.emit('loadstart');
+            _(this).state.readyState = 3;
+        });
+        const checkIfCanPlayThrough = (data => {
+            const percent = parseFloat(data.percent);
+
+            if (percent >= 0.25) {
+                this.emit('canplaythrough');
+                _(this).state.readyState = 4;
+                player.removeListener('loadProgress', checkIfCanPlayThrough);
+            }
+        });
+        const onLoadProgress = (data => {
+            const percent = parseFloat(data.percent);
+
+            _(this).state.buffered = percent;
+            this.emit('progress');
+        });
+        const onFinish = (() => {
+            this.emit('ended');
+            _(this).state.ended = true;
+        });
+        const onPause = (() => {
+            this.emit('pause');
+            _(this).state.paused = true;
+        });
+        const onPlay = (() => {
+            const {state} = _(this);
+
+            if (state.ended) {
+                player.call('seekTo', start);
+            }
+
+            this.emit('play');
+            state.ended = false;
+            state.paused = false;
+
+            state.hasPlayed = true;
+        });
+        const onSeek = (() => {
+            this.emit('seeked');
+            _(this).state.seeking = false;
+        });
+        const onPlayProgress = (({ seconds }) => {
+            const time = parseFloat(seconds);
+            const {state} = _(this);
+
+            state.currentTime = Math.min(
+                Math.max(time - start, 0),
+                end - start
+            );
+
+            if (time < start) {
+                return player.call('seekTo', start);
+            } else if (time >= end) {
+                state.ended = true;
+                this.emit('ended');
+                return player.call('pause');
+            }
+
+            this.emit('timeupdate');
+        });
+
+        player.once('ready', () => {
+            player.once('loadProgress', onceLoadProgress);
+            player.on('loadProgress', checkIfCanPlayThrough);
+            player.on('loadProgress', onLoadProgress);
+            player.on('finish', onFinish);
+            player.on('pause', onPause);
+            player.on('play', onPlay);
+            player.on('seek', onSeek);
+            player.on('playProgress', onPlayProgress);
+
+            player.call('getDuration').then(duration => {
+                const {state} = _(this);
+
+                this.emit('loadedmetadata');
+                state.readyState = 1;
+                state.duration = (Math.min(end, duration) -  start);
+            });
+
+            _(this).state.ready = true;
+        });
+
+        Runner.schedule('afterRender', () => element.appendChild(iframe));
+
+        _(this).iframe = iframe;
+        _(this).player = player;
+    }
+
+    unload() {
+        const {player, iframe} = _(this);
+        if (!player) { return; }
+
+        player.destroy();
+        this.element.removeChild(iframe);
+        _(this).state = getInitialState();
+
+        _(this).iframe = null;
+        _(this).player = null;
+    }
+
+    reload() {
+        this.unload();
+        this.load();
+    }
+
+    minimize() {
+        return new Error('The video cannot be minimized.');
+    }
+
+    didInsertElement() {
+        if (this.autoplay) { this.play(); }
+
+        return super(...arguments);
+    }
+}

634802a4544ce22c54df4dde33c333a76325b5f2
howard <howard.engelhart@outlook.com>
2015-03-23 13:12:27 -0400

    more moat functionality implemented into the service.

--- src/services/moat.js
+++ src/services/moat.js
@@ -7,7 +7,6 @@ const _ = createKey();
 export default class MoatApi {
     constructor(container, ids, duration, url, partnerCode) {
         if (global.__karma__) { this._private_ = _(this); }
-
         const protocol = global.location.protocol;
         const sub = (protocol === 'https:') ? 'z' : 'js';
 
@@ -36,11 +35,28 @@ export default class MoatApi {
 
         _(this).name = "_moatApi" + Math.floor(Math.random() * 100000000);
 
+        _(this).src = protocol + '//' + sub +
+            '.moatads.com/cinema6748895486244/moatvideo.js#' + _(this).name;
+
         global[_(this).name] = _(this).tracker;
 
         if (container && container.childNodes){
-            
+            codeLoader.configure(_(this).name, {
+                src: _(this).src,
+                after() { return; }
+            });
+        
+            codeLoader.load(_(this).name,container,'insertBefore',
+                container.childNodes[0] || null);
         }
+    }
+
+    get name() {
+        return _(this).name;
+    }
+
+    dispatchEvent(...params) {
+        return _(this).tracker.dispatchEvent(...params);
     }
 
 };
--- test/unit/spec/moat.ut.js
+++ test/unit/spec/moat.ut.js
@@ -4,22 +4,22 @@ import codeLoader from '../../../src/services/code_loader.js';
 //import environment from '../../../src/environment.js';
 
 fdescribe('moat', function() {
-    describe('constructor', function() {
-        let api, container, ids;
-        beforeEach(function(){
-            container = { };
-            ids = {
-                'level1'    : '_ADVERTISER_',
-                'level2'    : '_LINE_ITEM_',
-                'level3'    : '_CREATIVE_',
-                'slicer1'   : '_SITE_',
-                'slicer2'   : '_PLACEMENT'
-            };
-            spyOn(Math,'random').and.returnValue(1);
-            spyOn(codeLoader,'configure');
-            spyOn(codeLoader,'load');
-        });
+    let api, container, ids;
+    beforeEach(function(){
+        container = { };
+        ids = {
+            'level1'    : '_ADVERTISER_',
+            'level2'    : '_LINE_ITEM_',
+            'level3'    : '_CREATIVE_',
+            'slicer1'   : '_SITE_',
+            'slicer2'   : '_PLACEMENT'
+        };
+        spyOn(Math,'random').and.returnValue(1);
+        spyOn(codeLoader,'configure');
+        spyOn(codeLoader,'load');
+    });
 
+    describe('constructor', function() {
         it('sets up the tracker',function(){
             expect(global._moatApi100000000).not.toBeDefined();
             api = new MoatApi(container,ids,30,'http://somewhere.com');
@@ -29,11 +29,49 @@ fdescribe('moat', function() {
             expect(api._private_.tracker.adData.duration).toEqual(30);
             expect(api._private_.tracker.adData.url).toEqual('http://somewhere.com');
             expect(api._private_.name).toEqual('_moatApi100000000');
+            expect(api._private_.src).toEqual('http://js.moatads.com/cinema6748895486244/moatvideo.js#_moatApi100000000');
             expect(global._moatApi100000000).toBeDefined();
+            expect(codeLoader.configure).not.toHaveBeenCalled();
+            expect(codeLoader.load).not.toHaveBeenCalled();
         });
 
-        it('initiates the loading of the moat library',function(){
+        it('initiates the loading of the moat library with valid container',function(){
+            container.childNodes = [];
+            api = new MoatApi(container,ids,30,'http://somewhere.com');
+            expect(codeLoader.configure).toHaveBeenCalled();
+            expect(codeLoader.load).toHaveBeenCalledWith(
+                '_moatApi100000000',container,'insertBefore',null
+            );
+        });
 
+    });
+
+    describe('properties', function(){
+        beforeEach(function(){
+            api = new MoatApi(container,ids,30,'http://somewhere.com');
+        });
+
+        it('name',function(){
+            expect(api.name).toEqual('_moatApi100000000');
+            expect(function(){
+                api.name = 'howard';
+            }).toThrow();
+        });
+
+    });
+
+    describe('dispatchEvent', function(){
+        beforeEach(function(){
+            api = new MoatApi(container,ids,30,'http://somewhere.com');
+        });
+
+        it('proxies calls to the tracker.dispatchEvent',function(){
+            spyOn(api._private_.tracker,'dispatchEvent');
+            api.dispatchEvent(1,2,3);
+            expect(api._private_.tracker.dispatchEvent).toHaveBeenCalledWith(1,2,3);
+        });
+
+        it('caches events before the moat lib is fully loaded',function(){
 
         });
 

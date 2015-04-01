865b2ec5b955757ed2f9d469c6a2c4037eca87e2
Joshua Minzner <josh@minzner.org>
2015-04-01 16:01:48 -0400

    Load full mode hover stylesheet if the device has a mouse.

--- src/controllers/full/FullApplicationController.js
+++ src/controllers/full/FullApplicationController.js
@@ -1,9 +1,15 @@
 import ApplicationController from '../ApplicationController.js';
 import FullPlayerController from './FullPlayerController.js';
+import browser from '../../services/browser.js';
+import codeLoader from '../../services/code_loader.js';
 
 export default class FullApplicationController extends ApplicationController {
     constructor() {
         super(...arguments);
+
+        browser.test('mouse').then(hasMouse => {
+            if (hasMouse) { codeLoader.loadStyles('css/full--hover.css'); }
+        });
 
         this.PlayerCtrl = new FullPlayerController(this.appView);
     }

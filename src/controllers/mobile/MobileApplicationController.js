9d311c209e83e569ba087aea7824b3046ded3be3
Joshua Minzner <josh@minzner.org>
2015-04-07 10:14:36 -0400

    Only include GSAP plugin in mobile build.

--- src/controllers/mobile/MobileApplicationController.js
+++ src/controllers/mobile/MobileApplicationController.js
@@ -1,11 +1,12 @@
 import ApplicationController from '../ApplicationController.js';
 import MobilePlayerController from './MobilePlayerController.js';
+import 'gsap/src/uncompressed/plugins/CSSPlugin.js';
+import '../../animations/mobile.js';
 
 export default class MobileApplicationController extends ApplicationController {
     constructor() {
         super(...arguments);
 
-        require('../../animations/mobile.js');
         this.PlayerCtrl = new MobilePlayerController(this.appView);
     }
 }

3eda50709dfaf0426264cf980335338b8cabeb51
Joshua Minzner <josh@minzner.org>
2015-04-07 12:14:21 -0400

    Move playlist/resizing logic out of the FullPlayerController and into a mixin.

---
+++ src/mixins/PlaylistPlayerController.js
@@ -0,0 +1,42 @@
+import PlaylistViewController from '../controllers/PlaylistViewController.js';
+import DisplayAdController from '../controllers/DisplayAdController.js';
+
+function PlaylistPlayerController() {}
+PlaylistPlayerController.prototype = {
+    initPlaylist: function() {
+        this.PlaylistViewCtrl = new PlaylistViewController(this.minireel);
+        this.DisplayAdCtrl = new DisplayAdController();
+
+        this.minireel.once('init', () => {
+            this.PlaylistViewCtrl.renderInto(this.view.playlistOutlet);
+            this.DisplayAdCtrl.renderInto(this.view.displayAdOutlet);
+        });
+        this.minireel.on('becameUnskippable', () => this.PlaylistViewCtrl.disable());
+        this.minireel.on('becameSkippable', () => this.PlaylistViewCtrl.enable());
+        this.minireel.on('move', () => {
+            const { currentCard } = this.minireel;
+            if (!currentCard) { return; }
+            const { type, modules: { displayAd } } = currentCard;
+
+            if (displayAd) {
+                this.DisplayAdCtrl.model = displayAd;
+                this.DisplayAdCtrl.activate();
+            } else {
+                this.DisplayAdCtrl.deactivate();
+            }
+
+            if (type === 'recap') {
+                this.view.expand();
+                this.PlaylistViewCtrl.hide();
+            } else {
+                this.view.contract();
+                this.PlaylistViewCtrl.show();
+            }
+        });
+
+        this.DisplayAdCtrl.on('activate', () => this.PlaylistViewCtrl.contract());
+        this.DisplayAdCtrl.on('deactivate', () => this.PlaylistViewCtrl.expand());
+    }
+};
+
+export default PlaylistPlayerController;

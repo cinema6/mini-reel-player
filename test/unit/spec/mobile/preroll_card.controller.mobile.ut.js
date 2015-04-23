a39630f27638c63bb27154886c6ce6d8b55c14fc
Joshua Minzner <josh@minzner.org>
2015-04-23 14:14:51 -0400

    Add View/Controller for the preroll card in the mobile player.

---
+++ test/unit/spec/mobile/preroll_card.controller.mobile.ut.js
@@ -0,0 +1,28 @@
+import MobilePrerollCardController from '../../../../src/controllers/mobile/MobilePrerollCardController.js';
+import PrerollCardController from '../../../../src/controllers/PrerollCardController.js';
+import { EventEmitter } from 'events';
+import MobilePrerollCardView from '../../../../src/views/mobile/MobilePrerollCardView.js';
+
+describe('MobilePrerollCardController', function() {
+    let MobilePrerollCardCtrl;
+    let card;
+
+    beforeEach(function() {
+        card = new EventEmitter();
+        card.data = {};
+
+        MobilePrerollCardCtrl = new MobilePrerollCardController(card);
+    });
+
+    it('should exist', function() {
+        expect(MobilePrerollCardCtrl).toEqual(jasmine.any(PrerollCardController));
+    });
+
+    describe('properties:', function() {
+        describe('view', function() {
+            it('should be a MobilePrerollCardView', function() {
+                expect(MobilePrerollCardCtrl.view).toEqual(jasmine.any(MobilePrerollCardView));
+            });
+        });
+    });
+});

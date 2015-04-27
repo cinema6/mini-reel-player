345576a9ebdf33221d64d246619b9d4671a0ab75
Joshua Minzner <josh@minzner.org>
2015-04-27 15:55:01 -0400

    Consolidate hover styles logic in the base ApplicationController.

--- src/controllers/lightbox-playlist/LightboxPlaylistApplicationController.js
+++ src/controllers/lightbox-playlist/LightboxPlaylistApplicationController.js
@@ -1,16 +1,10 @@
 import ApplicationController from '../ApplicationController.js';
 import LightboxPlaylistPlayerController from './LightboxPlaylistPlayerController.js';
-import browser from '../../services/browser.js';
-import codeLoader from '../../services/code_loader.js';
 
 export default class LightboxPlaylistApplicationController extends ApplicationController {
     constructor() {
         super(...arguments);
 
         this.PlayerCtrl = new LightboxPlaylistPlayerController(this.appView);
-
-        browser.test('mouse').then(hasMouse => {
-            if (hasMouse) { codeLoader.loadStyles('css/lightbox-playlist--hover.css'); }
-        });
     }
 }
--- test/unit/spec/application.controller.ut.js
+++ test/unit/spec/application.controller.ut.js
@@ -1,20 +1,66 @@
+import ApplicationController from '../../../src/controllers/ApplicationController.js';
+import ApplicationView from '../../../src/views/ApplicationView.js';
+import Controller from '../../../lib/core/Controller.js';
+import Runner from '../../../lib/Runner.js';
+import environment from '../../../src/environment.js';
+import browser from '../../../src/services/browser.js';
+import codeLoader from '../../../src/services/code_loader.js';
+import RunnerPromise from '../../../lib/RunnerPromise.js';
+import {
+    defer
+} from '../../../lib/utils.js';
+
 describe('ApplicationController', function() {
-    import ApplicationController from '../../../src/controllers/ApplicationController.js';
-    import ApplicationView from '../../../src/views/ApplicationView.js';
-    import Controller from '../../../lib/core/Controller.js';
-    import Runner from '../../../lib/Runner.js';
     let ApplicationCtrl;
+    let mouseDeferred;
 
     let root;
 
     beforeEach(function() {
+        environment.constructor();
         root = document.createElement('body');
+        mouseDeferred = defer(RunnerPromise);
+        spyOn(browser, 'test').and.returnValue(mouseDeferred.promise);
+
+        environment.mode = 'some-mode';
 
         Runner.run(() => ApplicationCtrl = new ApplicationController(root));
     });
 
+    afterAll(function() {
+        environment.constructor();
+    });
+
     it('should exist', function() {
         expect(ApplicationCtrl).toEqual(jasmine.any(Controller));
+    });
+
+    it('should check to see if the device has a mouse', function() {
+        expect(browser.test).toHaveBeenCalledWith('mouse');
+    });
+
+    describe('if the device has a mouse', function() {
+        beforeEach(function(done) {
+            spyOn(codeLoader, 'loadStyles');
+            mouseDeferred.fulfill(true);
+            mouseDeferred.promise.then(done, done);
+        });
+
+        it('should load the hover styles', function() {
+            expect(codeLoader.loadStyles).toHaveBeenCalledWith(`css/${environment.mode}--hover.css`);
+        });
+    });
+
+    describe('if the device has no mouse', function() {
+        beforeEach(function(done) {
+            spyOn(codeLoader, 'loadStyles');
+            mouseDeferred.fulfill(false);
+            mouseDeferred.promise.then(done, done);
+        });
+
+        it('should not load the hover styles', function() {
+            expect(codeLoader.loadStyles).not.toHaveBeenCalled();
+        });
     });
 
     describe('properties:', function() {

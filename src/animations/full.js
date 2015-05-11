2276aab419196115b0f10a9ed213e965dec7a4bd
Joshua Minzner <josh@minzner.org>
2015-05-11 13:17:45 -0400

    Inplement crossfade card transition animation.

---
+++ src/animations/full.js
@@ -0,0 +1,42 @@
+import animator from '../../lib/animator.js';
+import TimelineLite from 'gsap/src/uncompressed/TimelineLite.js';
+
+let currentCard = null;
+const cardState = {};
+let showAnimation = null;
+
+function makeCardState(shown) {
+    return {
+        shown: shown
+    };
+}
+
+animator.on('card:show', function(card, done) {
+    currentCard = card;
+    const element = card.element;
+
+    showAnimation = new TimelineLite().set(element, {
+        display: '',
+        opacity: 0
+    }).to(element, 0.5, {
+        opacity: 1
+    }).call(function() {
+        showAnimation = null;
+    }).call(done);
+});
+animator.on('card:hide', function(card, done) {
+    if (!cardState[card.id]) {
+        cardState[card.id] = makeCardState(false);
+        return new TimelineLite().set(card.element, { display: 'none' }).call(done);
+    }
+
+    setTimeout(function() {
+        if (showAnimation) {
+            showAnimation.set(card.element, {
+                display: 'none'
+            }).call(done);
+        } else {
+            return new TimelineLite().set(card.element, { display: 'none' }).call(done);
+        }
+    }, 0);
+});
--- src/controllers/full/FullApplicationController.js
+++ src/controllers/full/FullApplicationController.js
@@ -1,5 +1,7 @@
 import ApplicationController from '../ApplicationController.js';
 import FullPlayerController from './FullPlayerController.js';
+import 'gsap/src/uncompressed/plugins/CSSPlugin.js';
+import '../../animations/full.js';
 
 export default class FullApplicationController extends ApplicationController {
     constructor() {

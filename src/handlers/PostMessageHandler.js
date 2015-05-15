9bcd51f3a91cfadec17baea2228fd7870bf4162c
Joshua Minzner <josh@minzner.org>
2015-05-15 11:36:37 -0400

    Add PostMessageHandler for emitting events from the player.

---
+++ src/handlers/PostMessageHandler.js
@@ -0,0 +1,24 @@
+import BillingHandler from './BillingHandler.js';
+
+export default class PostMessageHandler extends BillingHandler {
+    constructor(register, postMessage) {
+        super(...arguments);
+
+        const post = (data => postMessage.call(window.parent, JSON.stringify(data), '*'));
+
+        register(({ target: minireel }) => {
+            const firstCard = minireel.deck[0];
+
+            if (!firstCard) { return; }
+
+            post({
+                event: 'launch',
+                isSponsored: !!firstCard.sponsor,
+                isClickToPlay: !firstCard.data.autoplay
+            });
+        }, 'navigation', 'launch');
+
+        this.on('AdClick', () => post({ event: 'adStart' }));
+        this.on('AdCount', () => post({ event: 'adCount' }));
+    }
+}

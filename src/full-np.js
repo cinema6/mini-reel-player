4536f6a51285ee2c6f69c90174b38ed127cb0d48
Joshua Minzner <josh@minzner.org>
2015-05-28 07:54:47 -0400

    Remove card resizing from full-np player.

--- src/full-np.js
+++ src/full-np.js
@@ -1,9 +1,5 @@
 import './main.js';
 import Runner from '../lib/Runner.js';
 import FullNPApplicationController from './controllers/full-np/FullNPApplicationController.js';
-import CardView from './views/CardView.js';
-import ResizingCardView from './mixins/ResizingCardView.js';
-
-CardView.mixin(ResizingCardView);
 
 Runner.run(() => new FullNPApplicationController(document.body));

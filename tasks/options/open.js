79f186237b9e3614f9d1bd025021cee7b33779c9
Joshua Minzner <josh@minzner.org>
2015-06-30 12:04:51 -0400

    Add Grunt tasks for YUIDoc building/development/deployment.

--- tasks/options/open.js
+++ tasks/options/open.js
@@ -2,5 +2,9 @@ module.exports = {
     server: {
         url: 'http://localhost:<%= settings.port %>/',
         app: '<%= personal.browser %>'
+    },
+    docs: {
+        url: 'http://localhost:8000/',
+        app: '<%= personal.browser %>'
     }
 };

be31a57587a4336bbda5652a10500e5110be5aa6
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-06-30 12:33:23 -0400

    Removed article card from light player.

--- src/controllers/light/LightPlayerController.js
+++ src/controllers/light/LightPlayerController.js
@@ -1,7 +1,6 @@
 import PlayerController from '../PlayerController.js';
 import LightPlayerView from '../../views/light/LightPlayerView.js';
 import ThumbnailNavigatorPlayerController from '../../mixins/ThumbnailNavigatorPlayerController.js';
-import LightArticleCardController from './LightArticleCardController.js';
 import LightTextCardController from './LightTextCardController.js';
 import LightImageCardController from './LightImageCardController.js';
 import LightVideoCardController from './LightVideoCardController.js';
@@ -16,7 +15,6 @@ export default class LightPlayerController extends PlayerController {
 
         this.view = this.addView(new LightPlayerView());
         this.CardControllers = {
-            article: LightArticleCardController,
             text: LightTextCardController,
             image: LightImageCardController,
             video: LightVideoCardController,

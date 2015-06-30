3ac0f220f0a368bcd3c3a37322ecb7a3fadbe7f0
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-06-30 14:14:52 -0400

    Removed image cards from solo player.

--- src/controllers/solo/SoloPlayerController.js
+++ src/controllers/solo/SoloPlayerController.js
@@ -1,6 +1,5 @@
 import PlayerController from '../PlayerController.js';
 import SoloPlayerView from '../../views/solo/SoloPlayerView.js';
-import SoloImageCardController from './SoloImageCardController.js';
 import SoloVideoCardController from './SoloVideoCardController.js';
 import FullPrerollCardController from '../full/FullPrerollCardController.js';
 
@@ -9,7 +8,6 @@ export default class SoloPlayerController extends PlayerController {
         super(...arguments);
 
         this.CardControllers = {
-            image: SoloImageCardController,
             video: SoloVideoCardController,
             preroll: FullPrerollCardController
         };

f8af226d2158dd2dfb86050c59bbbaf95438c778
Moo H Kim <moo@halopowered.com>
2015-06-30 12:38:04 -0400

    article cards and image cards integration started

--- src/views/image_embeds/GettyEmbedView.js
+++ src/views/image_embeds/GettyEmbedView.js
@@ -4,6 +4,7 @@ export default class GettyEmbedView extends TemplateView {
     constructor() {
         super(...arguments);
         this.template = require('./GettyEmbedView.html');
+        this.classes.push('imgCard', 'imgCard--getty');
         this.tag = 'div';
     }
 }

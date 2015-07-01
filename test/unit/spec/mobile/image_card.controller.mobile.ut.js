4fef666248d03290b9cd3face71398dddf20148a
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-07-01 10:19:51 -0400

    Added separate image card template for mobile.

--- test/unit/spec/mobile/image_card.controller.mobile.ut.js
+++ test/unit/spec/mobile/image_card.controller.mobile.ut.js
@@ -1,7 +1,7 @@
 import MobileImageCardController from '../../../../src/controllers/mobile/MobileImageCardController.js';
 import ImageCardController from '../../../../src/controllers/ImageCardController.js';
 import View from '../../../../lib/core/View.js';
-import MobileVideoCardView from '../../../../src/views/mobile/MobileVideoCardView.js';
+import MobileImageCardView from '../../../../src/views/mobile/MobileImageCardView.js';
 import {EventEmitter} from 'events';
 
 describe('MobileImageCardController', function() {
@@ -23,8 +23,8 @@ describe('MobileImageCardController', function() {
 
     describe('properties:', function() {
         describe('view', function() {
-            it('should be a MobileVideoCardView', function() {
-                expect(MobileImageCardCtrl.view).toEqual(jasmine.any(MobileVideoCardView));
+            it('should be a MobileImageCardView', function() {
+                expect(MobileImageCardCtrl.view).toEqual(jasmine.any(MobileImageCardView));
                 expect(MobileImageCardCtrl.addView).toHaveBeenCalledWith(MobileImageCardCtrl.view);
             });
         });

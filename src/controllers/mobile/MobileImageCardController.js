1560f904d14045a9987082e0f1dc733d682ceaf7
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-07-01 10:19:51 -0400

    Added separate image card template for mobile.

--- src/controllers/mobile/MobileImageCardController.js
+++ src/controllers/mobile/MobileImageCardController.js
@@ -1,11 +1,11 @@
 import ImageCardController from '../ImageCardController.js';
-import MobileVideoCardView from '../../views/mobile/MobileVideoCardView.js';
+import MobileImageCardView from '../../views/mobile/MobileImageCardView.js';
 
 export default class MobileImageCardController extends ImageCardController {
     constructor() {
         super(...arguments);
 
-        this.view = this.addView(new MobileVideoCardView());
+        this.view = this.addView(new MobileImageCardView());
     }
 
     advance() {
---
+++ src/views/mobile/MobileImageCardView.js
@@ -0,0 +1,21 @@
+import VideoCardView from '../VideoCardView.js';
+import View from '../../../lib/core/View.js';
+import LinksListView from '../LinksListView.js';
+import PlayerOutletView from '../PlayerOutletView.js';
+import HideableView from '../HideableView.js';
+import ButtonView from '../ButtonView.js';
+
+export default class MobileImageCardView extends VideoCardView {
+    constructor() {
+        super(...arguments);
+
+        this.instantiates = {View, LinksListView, PlayerOutletView, HideableView, ButtonView};
+        this.template = require('./MobileImageCardView.html');
+    }
+
+    didCreateElement() {
+        super(...arguments);
+
+        this.replayContainer.hide();
+    }
+}

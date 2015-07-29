d6cdac1bc74ff548209536de34ac83ce01363c6b
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-07-29 14:44:15 -0400

    Added tests

--- test/unit/spec/mobile/player.controller.mobile.ut.js
+++ test/unit/spec/mobile/player.controller.mobile.ut.js
@@ -15,6 +15,8 @@ import View from '../../../../lib/core/View.js';
 import FullscreenPlayerController from '../../../../src/mixins/FullscreenPlayerController.js';
 import MobilePrerollCardController from '../../../../src/controllers/mobile/MobilePrerollCardController.js';
 import DisplayAdCardController from '../../../../src/controllers/DisplayAdCardController.js';
+import MobileInstagramImageCardController from '../../../../src/controllers/mobile/MobileInstagramImageCardController.js';
+import MobileInstagramVideoCardController from '../../../../src/controllers/mobile/MobileInstagramVideoCardController.js';
 import PrerollCard from '../../../../src/models/PrerollCard.js';
 
 describe('MobilePlayerController', function() {
@@ -74,6 +76,8 @@ describe('MobilePlayerController', function() {
                 expect(MobilePlayerCtrl.CardControllers.recap).toBe(MobileRecapCardController);
                 expect(MobilePlayerCtrl.CardControllers.preroll).toBe(MobilePrerollCardController);
                 expect(MobilePlayerCtrl.CardControllers.displayAd).toBe(DisplayAdCardController);
+                expect(MobilePlayerCtrl.CardControllers.instagramImage).toBe(MobileInstagramImageCardController);
+                expect(MobilePlayerCtrl.CardControllers.instagramVideo).toBe(MobileInstagramVideoCardController);
             });
         });
 

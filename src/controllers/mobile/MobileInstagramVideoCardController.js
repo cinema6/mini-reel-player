b6fcc867f9eb863f35c679eee2a847b586a40f02
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-08-03 11:08:48 -0400

    Added mock instagram video

--- src/controllers/mobile/MobileInstagramVideoCardController.js
+++ src/controllers/mobile/MobileInstagramVideoCardController.js
@@ -7,4 +7,8 @@ export default class MobileInstagramVideoCardController extends InstagramCardCon
 
         this.view = this.addView(new MobileInstagramVideoCardView());
     }
+
+    renderInstagram() {
+        
+    }
 }

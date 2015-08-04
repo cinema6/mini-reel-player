7cb2c694542368896fa920557e4f42553f56d158
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-08-04 12:12:59 -0400

    integrated instagram video card template for light player

--- src/views/light/LightInstagramVideoCardView.js
+++ src/views/light/LightInstagramVideoCardView.js
@@ -1,9 +1,11 @@
 import CardView from '../CardView.js';
+import View from '../../../lib/core/View.js';
 
 export default class LightInstagramVideoCardView extends CardView {
     constructor() {
         super(...arguments);
 
+        this.instantiates = { View };
         this.template = require('./LightInstagramVideoCardView.html');
     }
 }

ff6c64f0cdd3142008236306c4171ba18322a55d
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-08-04 11:50:09 -0400

    integrated the instagram video card template for the full player

--- src/models/InstagramVideoCard.js
+++ src/models/InstagramVideoCard.js
@@ -6,8 +6,22 @@ export default class InstagramVideoCard extends VideoCard {
         this.type = 'instagramVideo';
         this.title = data.caption;
         this.data.href = data.href;
+        this.data.src = data.data.src;
         this.user = {
-            href: data.user.href
+            href: data.user.href,
+            picture: data.user.picture,
+            username: data.user.username,
+            fullname: data.user.fullname,
+            bio: data.user.bio,
+            website: data.user.website,
+            posts: data.user.posts,
+            followers: data.user.followers,
+            following: data.user.following,
+            follow: data.user.follow
         };
+        this.likes = data.likes;
+        this.date = new Date(parseInt(data.date) * 1000);
+        this.caption = data.caption;
+        this.comments = data.comments;
     }
 }

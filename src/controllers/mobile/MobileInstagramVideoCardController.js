75159aad0866567c7f350176c52ed906345c4cfb
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-08-17 18:04:40 -0400

    created new templates for mobile instagram

--- src/controllers/mobile/MobileInstagramVideoCardController.js
+++ src/controllers/mobile/MobileInstagramVideoCardController.js
@@ -1,36 +1,14 @@
 import InstagramCardController from '../InstagramCardController.js';
-import MobileInstagramCardView from '../../views/mobile/MobileInstagramCardView.js';
-import InstagramEmbedView from '../../views/image_embeds/InstagramEmbedView.js';
-import { createKey } from 'private-parts';
-
-const _ = createKey();
+import MobileInstagramVideoCardView from '../../views/mobile/MobileInstagramVideoCardView.js';
 
 export default class MobileInstagramVideoCardController extends InstagramCardController {
     constructor() {
         super(...arguments);
 
-        _(this).embedView = null;
-        this.view = this.addView(new MobileInstagramCardView());
+        this.view = this.addView(new MobileInstagramVideoCardView());
     }
 
     renderInstagram() {
         this.isRendered = true;
-        if (!this.view.embedOutlet) {
-            this.view.create();
-        }
-
-        const embedView = new InstagramEmbedView();
-        embedView.update({
-            href: this.model.data.href,
-            caption: this.model.caption
-        });
-        this.view.embedOutlet.append(embedView);
-        _(this).embedView = embedView;
-    }
-
-    deactivate() {
-        _(this).embedView.destroy();
-        _(this).embedView = null;
-        this.isRendered = false;
     }
 }

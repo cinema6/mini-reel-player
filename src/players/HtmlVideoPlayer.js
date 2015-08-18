741561e7030d0021f2eeb3f8c304aad5a2408ec0
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-08-18 11:00:52 -0400

    fix skip to insta card with autoplay

--- src/players/HtmlVideoPlayer.js
+++ src/players/HtmlVideoPlayer.js
@@ -165,9 +165,9 @@ export default class HtmlVideoPlayer extends CorePlayer {
         video.addEventListener('error', _(this).error, false);
         video.addEventListener('ended', _(this).ended, false);
         video.addEventListener('timeupdate', _(this).timeupdate, false);
+        _(this).htmlVideo = video;
         Runner.schedule('afterRender', null, () => {
             element.appendChild(video);
-            _(this).htmlVideo = video;
         });
     }
 

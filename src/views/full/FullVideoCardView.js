f49395087c0c9f60b87c2e83304a922f644fc73f
Joshua Minzner <josh@minzner.org>
2015-09-11 18:07:22 -0400

    Update full/full-np players to use LinkItemView.

--- src/views/full/FullVideoCardView.js
+++ src/views/full/FullVideoCardView.js
@@ -4,13 +4,21 @@ import PlayerOutletView from '../PlayerOutletView.js';
 import SkipTimerView from '../SkipTimerView.js';
 import View from '../../../lib/core/View.js';
 import ButtonView from '../ButtonView.js';
+import LinkItemView from '../LinkItemView.js';
 
 export default class FullVideoCardView extends VideoCardView {
     constructor() {
         super(...arguments);
 
         this.template = require('./FullVideoCardView.html');
-        this.instantiates = {View, LinksListView, PlayerOutletView, SkipTimerView, ButtonView};
+        this.instantiates = {
+            View,
+            LinksListView,
+            PlayerOutletView,
+            SkipTimerView,
+            ButtonView,
+            LinkItemView
+        };
     }
 
     didCreateElement() {

4c6da4bf041929d4eede44f34f4fd8e3ac8c68f4
Joshua Minzner <josh@minzner.org>
2015-09-11 18:44:23 -0400

    Update mobile player to use LinkItemView.

--- src/views/mobile/MobileVideoCardView.js
+++ src/views/mobile/MobileVideoCardView.js
@@ -4,12 +4,20 @@ import LinksListView from '../LinksListView.js';
 import PlayerOutletView from '../PlayerOutletView.js';
 import HideableView from '../HideableView.js';
 import ButtonView from '../ButtonView.js';
+import LinkItemView from '../LinkItemView.js';
 
 export default class MobileVideoCardView extends VideoCardView {
     constructor() {
         super(...arguments);
 
-        this.instantiates = {View, LinksListView, PlayerOutletView, HideableView, ButtonView};
+        this.instantiates = {
+            View,
+            LinksListView,
+            PlayerOutletView,
+            HideableView,
+            ButtonView,
+            LinkItemView
+        };
         this.template = require('./MobileVideoCardView.html');
     }
 

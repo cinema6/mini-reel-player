c7c7ee761fcc38cd177840c834b67d9109edb755
Joshua Minzner <josh@minzner.org>
2015-09-11 19:40:08 -0400

    Update instagram cards to use data-model for LinkListViews.

--- src/views/InstagramCardView.js
+++ src/views/InstagramCardView.js
@@ -8,16 +8,4 @@ export default class InstagramCardView extends CardView {
 
         this.instantiates = { InstagramCaptionView, LinksListView };
     }
-
-    update(data) {
-        super(data);
-        if(data.sponsored) {
-            if(data.links && data.links.length > 0) {
-                this.links.update(data.links);
-                if(this.linksSmall) {
-                    this.linksSmall.update(data.links);
-                }
-            }
-        }
-    }
 }

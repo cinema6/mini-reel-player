e5a87b19c867f86c3f80f817869ded54bc7284e2
Joshua Minzner <josh@minzner.org>
2015-10-16 11:34:02 -0400

    Replace GSAP with moti.js.

--- src/controllers/mobile/MobileApplicationController.js
+++ src/controllers/mobile/MobileApplicationController.js
@@ -1,6 +1,5 @@
 import ApplicationController from '../ApplicationController.js';
 import MobilePlayerController from './MobilePlayerController.js';
-import 'gsap/src/uncompressed/plugins/CSSPlugin.js';
 import '../../animations/mobile.js';
 
 export default class MobileApplicationController extends ApplicationController {

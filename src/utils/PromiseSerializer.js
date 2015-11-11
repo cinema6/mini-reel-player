a2846e83f8acfc723903d6e3ac3e58d2ece287ac
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-11-11 12:56:31 -0500

    use noop in serializer

--- src/utils/PromiseSerializer.js
+++ src/utils/PromiseSerializer.js
@@ -1,4 +1,5 @@
 import {createKey} from 'private-parts';
+import {noop} from '../../lib/utils.js';
 
 const _ = createKey();
 
@@ -8,8 +9,8 @@ export default class PromiseSerializer {
 
         if (global.__karma__) { this.__private__ = _(this); }
     }
-    
+
     call(promiseFn) {
-        _(this).pending = _(this).pending.then(promiseFn).catch(() => {});
+        _(this).pending = _(this).pending.then(promiseFn).catch(noop);
     }
 }

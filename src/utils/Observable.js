15f06e86893454d576b0c0877550bbe4ae2294d1
Scott Bouloutian <ScottBouloutian@gmail.com>
2015-11-11 12:54:08 -0500

    added some events to observable

--- src/utils/Observable.js
+++ src/utils/Observable.js
@@ -12,34 +12,32 @@ export default class Observable extends EventEmitter {
 
         if (global.__karma__) { this.__private__ = _(this); }
     }
-    
+
     get(property) {
         return _(this).state[property];
     }
-    
+
     set(property, value) {
         const oldValue = _(this).state[property];
         if(oldValue !== undefined) {
             _(this).state[property] = value;
             const changed = (oldValue !== value);
-            const becameTrue = (changed && value === true);
-            const becameFalse = (changed && value === false);
+            const becameNull = (changed && value === null);
+            const becameUndefined = (changed && value === undefined);
+            const increased = (value > oldValue);
+            const decreased = (value < oldValue);
             const events = [];
-            if(changed) {
-                events.push('change');
-            }
-            if(becameTrue) {
-                events.push('becameTrue');
-            }
-            if(becameFalse) {
-                events.push('becameFalse');
-            }
+            if(changed) { events.push('change'); }
+            if(becameNull) { events.push('becameNull'); }
+            if(becameUndefined) { events.push('becameUndefined'); }
+            if(increased) { events.push('increased'); }
+            if(decreased) { events.push('decreased'); }
             events.forEach(eventName => {
                 this.emit(eventName + ':' + property, value);
             });
         }
     }
-    
+
     reset() {
         _(this).state = extend(_(this).defaultState);
     }
--- test/unit/spec/observable.ut.js
+++ test/unit/spec/observable.ut.js
@@ -6,61 +6,70 @@ describe('Observable', function() {
     beforeEach(function() {
         config = {
             prop1: 'foo',
-            prop2: 'bar'
+            prop2: 'bar',
+            count: 123
         };
         observable = new Observable(config);
         spyOn(observable, 'emit');
     });
-    
+
     it('should exist', function() {
         expect(observable).toEqual(jasmine.any(Observable));
     });
-    
+
     it('should set the state as a copy', function() {
         expect(observable.__private__.state).not.toBe(config);
         expect(observable.__private__.state).toEqual(config);
     });
-    
+
     it('should set the default state as a copy', function() {
         expect(observable.__private__.defaultState).not.toBe(config);
         expect(observable.__private__.defaultState).toEqual(config);
     });
-        
+
     it('should be able to get properties', function() {
         expect(observable.get('prop1')).toBe('foo');
         expect(observable.get('prop2')).toBe('bar');
         expect(observable.get('nonexistant')).not.toBeDefined();
     });
-    
+
     describe('setting properties', function() {
         it('should be able to set configured proeprties', function() {
             observable.set('prop1', 'new value');
             expect(observable.get('prop1')).toBe('new value');
         });
-        
+
         it('should not be able to set unconfigured properties', function() {
             observable.set('unknown', 'some value');
             expect(observable.get('unknown')).not.toBeDefined();
         });
-        
+
         it('should be able to emit changed events', function() {
             observable.set('prop1', 'new value');
             expect(observable.emit).toHaveBeenCalledWith('change:prop1', 'new value');
         });
-        
-        it('should be able to emit becameTrue events', function() {
-            observable.set('prop1', true);
-            expect(observable.emit).toHaveBeenCalledWith('change:prop1', true);
-            expect(observable.emit).toHaveBeenCalledWith('becameTrue:prop1', true);
+
+        it('should be able to emit becameNull events', function() {
+            observable.set('prop1', null);
+            expect(observable.emit).toHaveBeenCalledWith('becameNull:prop1', null);
         });
-        
-        it('should be able to emit becameFalse events', function() {
-            observable.set('prop1', false);
-            expect(observable.emit).toHaveBeenCalledWith('change:prop1', false);
-            expect(observable.emit).toHaveBeenCalledWith('becameFalse:prop1', false);
+
+        it('should be able to emit becameUndefined events', function() {
+            observable.set('prop1', undefined);
+            expect(observable.emit).toHaveBeenCalledWith('becameUndefined:prop1', undefined);
+        });
+
+        it('should be able to emit increased events', function() {
+            observable.set('count', 999);
+            expect(observable.emit).toHaveBeenCalledWith('increased:count', 999);
+        });
+
+        it('should be able to emit decreased events', function() {
+            observable.set('count', -999);
+            expect(observable.emit).toHaveBeenCalledWith('decreased:count', -999);
         });
     });
-    
+
     it('should be able to reset the state', function() {
         observable.set('prop1', 'new value');
         expect(observable.__private__.state).not.toEqual(config);

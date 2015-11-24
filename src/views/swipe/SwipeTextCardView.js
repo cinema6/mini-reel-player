625c455068176a9f9e2103b34153ea99648211bc
Joshua Minzner <josh@minzner.org>
2015-11-24 11:19:24 -0500

    Deprecate swipe player.

--- src/views/swipe/SwipeTextCardView.js
+++
@@ -1,9 +0,0 @@
-import CardView from '../CardView.js';
-
-export default class SwipeTextCardView extends CardView {
-    constructor() {
-        super(...arguments);
-
-        this.template = require('./SwipeTextCardView.html');
-    }
-}

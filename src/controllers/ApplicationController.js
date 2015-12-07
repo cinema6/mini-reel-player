fdaff5906461fffafc7389bbbca46c16f2789c7f
Joshua Minzner <josh@minzner.org>
2015-12-07 15:06:46 -0500

    Don't render in calls to requestAnimationFrame() when the context is 'mraid.'

--- src/controllers/ApplicationController.js
+++ src/controllers/ApplicationController.js
@@ -2,11 +2,23 @@ import Controller from '../../lib/core/Controller.js';
 import browser from '../../src/services/browser.js';
 import codeLoader from '../../src/services/code_loader.js';
 import environment from '../environment.js';
+import Runner from '../../lib/Runner.js';
 
 import ApplicationView from '../views/ApplicationView.js';
 
 export default class ApplicationController extends Controller {
     constructor(root) {
+        // If we're running in MRAID, we want to be able to render the player when the UIWebView is
+        // off-screen. To do that, we can't render in calls to requestAnimationFrame(), so we're
+        // giving the RenderQueue the same flush() method as the BeforeRenderQueue() (which does
+        // not use RAF.)
+        if (environment.params.context === 'mraid') {
+            const BeforeRenderQueue = Runner.queues[0];
+            const RenderQueue = Runner.queues[1];
+
+            RenderQueue.prototype.flush = BeforeRenderQueue.prototype.flush;
+        }
+
         super(...arguments);
 
         this.appView = new ApplicationView(root);

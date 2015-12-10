2c75bf9663ec579b1f910568228be344b60b77d7
Joshua Minzner <josh@minzner.org>
2015-12-10 10:23:48 -0500

    Refactor pixel-firing logic in the ADTECHHandler.

--- src/handlers/ADTECHHandler.js
+++ src/handlers/ADTECHHandler.js
@@ -25,21 +25,24 @@ function completeUrlWithLoadDelay() {
     };
 }
 
+function firePixels(campaign, type, mapper) {
+    const pixels = campaign[type];
+
+    if (pixels) { imageLoader.load(...map(pixels, mapper)); }
+}
+
 export default class ADTECHHandler extends BillingHandler {
     constructor(register) {
         super(...arguments);
 
         register(({ target: minireel }) => {
-            const { launchUrls } = minireel.campaign;
             const { loadStartTime } = environment;
             const launchDelay = loadStartTime && (Date.now() - loadStartTime);
 
-            if (launchUrls) {
-                imageLoader.load(...map(launchUrls, url => completeUrl(url, {
-                    '{launchDelay}': launchDelay,
-                    '{delay}': launchDelay
-                })));
-            }
+            firePixels(minireel.campaign, 'launchUrls', url => completeUrl(url, {
+                '{launchDelay}': launchDelay,
+                '{delay}': launchDelay
+            }));
         }, 'navigation', 'launch');
         register(({ target: minireel }) => {
             const { loadStartTime } = environment;
@@ -51,58 +54,40 @@ export default class ADTECHHandler extends BillingHandler {
         }, 'navigation', 'init');
 
         register(({ data: card }) => {
-            const { bufferUrls } = card.campaign;
-
-            if (bufferUrls) { imageLoader.load(...map(bufferUrls, completeUrlWithLoadDelay())); }
+            firePixels(card.campaign, 'bufferUrls', completeUrlWithLoadDelay());
         }, 'video', 'buffering');
         register(({ data: card }) => {
-            const { q1Urls } = card.campaign;
-
-            if (q1Urls) { imageLoader.load(...map(q1Urls, completeUrlWithCardViewDelay(card))); }
+            firePixels(card.campaign, 'q1Urls', completeUrlWithCardViewDelay(card));
         }, 'video', 'firstQuartile');
         register(({ data: card }) => {
-            const { q2Urls } = card.campaign;
-
-            if (q2Urls) { imageLoader.load(...map(q2Urls, completeUrlWithCardViewDelay(card))); }
+            firePixels(card.campaign, 'q2Urls', completeUrlWithCardViewDelay(card));
         }, 'video', 'midpoint');
         register(({ data: card }) => {
-            const { q3Urls } = card.campaign;
-
-            if (q3Urls) { imageLoader.load(...map(q3Urls, completeUrlWithCardViewDelay(card))); }
+            firePixels(card.campaign, 'q3Urls', completeUrlWithCardViewDelay(card));
         }, 'video', 'thirdQuartile');
         register(({ data: card }) => {
-            const { q4Urls } = card.campaign;
-
-            if (q4Urls) { imageLoader.load(...map(q4Urls, completeUrlWithCardViewDelay(card))); }
+            firePixels(card.campaign, 'q4Urls', completeUrlWithCardViewDelay(card));
         }, 'video', 'complete');
 
         register(({ target: card }) => {
-            const { viewUrls } = card.campaign;
-
-            if (viewUrls) { imageLoader.load(...map(viewUrls, completeUrlWithLoadDelay())); }
+            firePixels(card.campaign, 'viewUrls', completeUrlWithLoadDelay());
         }, 'card', 'activate');
         register(({ target: card }, { tracking }) => {
             imageLoader.load(...map(tracking, completeUrlWithCardViewDelay(card)));
         }, 'card', 'clickthrough', 'share');
 
         this.on('AdClick', card => {
-            const { campaign: { playUrls }, lastViewedTime } = card;
+            const { lastViewedTime } = card;
             const playDelay = Date.now() - lastViewedTime;
 
-            if (playUrls) {
-                imageLoader.load(...map(playUrls, url => completeUrl(url, {
-                    '{playDelay}': playDelay,
-                    '{delay}': playDelay
-                })));
-            }
+            firePixels(card.campaign, 'playUrls', url => completeUrl(url, {
+                '{playDelay}': playDelay,
+                '{delay}': playDelay
+            }));
         });
 
         this.on('AdCount', card => {
-            const { countUrls } = card.campaign;
-
-            if (countUrls) {
-                imageLoader.load(...map(countUrls, completeUrlWithCardViewDelay(card)));
-            }
+            firePixels(card.campaign, 'countUrls', completeUrlWithCardViewDelay(card));
         });
     }
 }

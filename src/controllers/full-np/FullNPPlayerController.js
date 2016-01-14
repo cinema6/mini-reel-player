a92189f050921e588e7921218930302b63caecf7
Joshua Minzner <josh@minzner.org>
2016-01-14 15:27:37 -0500

    Add conditional build logic to the FullNPPlayerController.

--- src/controllers/full-np/FullNPPlayerController.js
+++ src/controllers/full-np/FullNPPlayerController.js
@@ -1,11 +1,30 @@
 import PlayerController from '../PlayerController.js';
 import FullNPPlayerView from '../../views/full-np/FullNPPlayerView.js';
-import FullNPImageCardController from './FullNPImageCardController.js';
+
+/***************************************************************************************************
+ * CARD CONTROLLER IMPORTS
+ **************************************************************************************************/
 import FullNPVideoCardController from './FullNPVideoCardController.js';
-import FullNPRecapCardController from './FullNPRecapCardController.js';
+
+/* #if card.types.indexOf('image') > -1 */
+import FullNPImageCardController from './FullNPImageCardController.js';
+/* #endif */
+
+/* #if card.types.indexOf('instagram') > -1 */
 import FullNPInstagramImageCardController from './FullNPInstagramImageCardController.js';
 import FullNPInstagramVideoCardController from './FullNPInstagramVideoCardController.js';
+/* #endif */
+
+/* #if card.types.indexOf('recap') > -1 */
+import FullNPRecapCardController from './FullNPRecapCardController.js';
+/* #endif */
+
+/***************************************************************************************************
+ * MIXIN IMPORTS
+ **************************************************************************************************/
+/* #if isMiniReel */
 import ThumbnailNavigatorPlayerController from '../../mixins/ThumbnailNavigatorPlayerController.js';
+/* #endif */
 
 export default class FullNPPlayerController extends PlayerController {
     constructor() {
@@ -14,14 +33,25 @@ export default class FullNPPlayerController extends PlayerController {
         this.view = this.addView(new FullNPPlayerView());
 
         this.CardControllers = {
-            image: FullNPImageCardController,
             video: FullNPVideoCardController,
-            recap: FullNPRecapCardController,
+
+            /* #if card.types.indexOf('image') > -1 */
+            image: FullNPImageCardController,
+            /* #endif */
+
+            /* #if card.types.indexOf('instagram') > -1 */
             instagramImage: FullNPInstagramImageCardController,
-            instagramVideo: FullNPInstagramVideoCardController
+            instagramVideo: FullNPInstagramVideoCardController,
+            /* #endif */
+
+            /* #if card.types.indexOf('recap') > -1 */
+            recap: FullNPRecapCardController,
+            /* #endif */
         };
 
+        /* #if isMiniReel */
         this.initThumbnailNavigator();
+        /* #endif */
     }
 
     updateView() {
@@ -29,4 +59,6 @@ export default class FullNPPlayerController extends PlayerController {
         return super.updateView(...arguments);
     }
 }
+/* #if isMiniReel */
 FullNPPlayerController.mixin(ThumbnailNavigatorPlayerController); // jshint ignore:line
+/* #endif */

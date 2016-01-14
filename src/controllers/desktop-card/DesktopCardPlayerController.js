c3a24fe1d58dac6a72401ce8ef2a9c4691514480
Joshua Minzner <josh@minzner.org>
2016-01-14 15:17:23 -0500

    Add conditional build logic to the DesktopCardPlayerController.

--- src/controllers/desktop-card/DesktopCardPlayerController.js
+++ src/controllers/desktop-card/DesktopCardPlayerController.js
@@ -1,8 +1,15 @@
 import CardPlayerController from '../CardPlayerController.js';
-import DesktopCardVideoCardController from './DesktopCardVideoCardController.js';
 import DesktopCardPlayerView from '../../views/desktop-card/DesktopCardPlayerView.js';
+
+/***************************************************************************************************
+ * CARD CONTROLLER IMPORTS
+ **************************************************************************************************/
+import DesktopCardVideoCardController from './DesktopCardVideoCardController.js';
+
+/* #if card.types.indexOf('instagram') > -1 */
 import LightInstagramImageCardController from '../light/LightInstagramImageCardController.js';
 import LightInstagramVideoCardController from '../light/LightInstagramVideoCardController.js';
+/* #endif */
 
 export default class DesktopCardPlayerController extends CardPlayerController {
     constructor(rootView) {
@@ -10,8 +17,11 @@ export default class DesktopCardPlayerController extends CardPlayerController {
 
         this.CardControllers = {
             video: DesktopCardVideoCardController,
+
+            /* #if card.types.indexOf('instagram') > -1 */
             instagramImage: LightInstagramImageCardController,
-            instagramVideo: LightInstagramVideoCardController
+            instagramVideo: LightInstagramVideoCardController,
+            /* #endif */
         };
 
         this.view = this.addView(new DesktopCardPlayerView());

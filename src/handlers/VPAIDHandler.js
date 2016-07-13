00d3996141a8f6600032e2e9f0067da242460b24
Joshua Minzner <josh@minzner.org>
2016-07-13 10:58:28 -0400

    Udpate jshint.

--- src/handlers/VPAIDHandler.js
+++ src/handlers/VPAIDHandler.js
@@ -7,12 +7,7 @@ export default class VPAIDHandler extends BillingHandler {
         });
         const videoCall = ((() => {
             const queue = [];
-
             let video;
-            register(({ target }) => {
-                video = target;
-                flush();
-            }, 'video', '@addSource');
 
             function flush() {
                 let item;
@@ -20,6 +15,11 @@ export default class VPAIDHandler extends BillingHandler {
                     video[item.method](...item.args);
                 }
             }
+
+            register(({ target }) => {
+                video = target;
+                flush();
+            }, 'video', '@addSource');
 
             return function videoCall(method, ...args) {
                 queue.push({ method, args });
